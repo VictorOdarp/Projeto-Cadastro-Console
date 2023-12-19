c1abd6538a2f5a89c86125e0c161d8fe2c6eb0b6
Victor Eduardo <VictorOdarp@gmail.com>
2023-12-18 21:41:32 -0300

    Classe User, Salvar usuarios em arquivos.txt, transferir dados escritos em console para arquivos.txt

---
+++ ProjetoCadastro/ProjetoCadastro/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace ProjetoCadastro.Entities
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+        public double Height { get; set; }
+
+        public User()
+        {
+
+        }
+        
+        public User(string name, string email, int year, double height)
+        {
+            Name = name;
+            Email = email;
+            Year = year;
+            Height = height;
+        }
+
+
+    }
+}
---
+++ ProjetoCadastro/ProjetoCadastro/Program.cs
@@ -0,0 +1,83 @@
+﻿using ProjetoCadastro.Entities;
+using System;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProjetoCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\Victo\Desktop\Projeto Cadastro\Formulario.txt";
+            User user = new User();
+            List<User> list = new List<User>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ex.Message);
+            }
+
+            CadastrarUsuario();
+
+            void CadastrarUsuario()
+            {
+                int count = list.Count; 
+                
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Year: ");
+                int year = int.Parse(Console.ReadLine());
+                Console.Write("Height: ");
+                double height = double.Parse(Console.ReadLine());
+                User user = new User(name, email, year, height);
+                list.Add(user);
+
+                string targetPath = (@"C:\Users\Victo\Desktop\Projeto Cadastro\" + (count + 1) + "-" + user.Name + ".txt");
+
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(targetPath))
+                    {
+                        sw.WriteLine("Name: " + user.Name);
+                        sw.WriteLine("Email: " + user.Email);
+                        sw.WriteLine("Year: " + user.Year);
+                        sw.WriteLine("Heigh: " + user.Height);
+                        Console.WriteLine("Usuário cadastrado com sucesso!");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            CadastrarUsuario();
+
+
+
+
+
+
+
+        }
+    }
+}
